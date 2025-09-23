fded405df50e5fdb82be38f29392abefee5f38fd
vanguleprince <vangu.leprince@gmail.com>
2025-09-23 15:02:09 +0100

    feat i'm done my secon exercices with the data type

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,13 @@
 // Exercice 6
 // Écrivez votre code ici
+let Age = 24
+let Nom = "Vangu-Mvuluzi"
+let Connected = false
+let valeurNulle = "Null"
+let Inconnue
+
+console.log(Age);
+console.log(Nom);
+console.log(Connected);
+console.log(valeurNulle);
+console.log(Inconnue);
