c9bfa4856295c86910ed9d89c5bcf9fa19d6b9cd
vanguleprince <vangu.leprince@gmail.com>
2025-09-23 16:36:36 +0100

    feat it let's me two exercises to do

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,12 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant= 12
+let agePre = 36
+let ageMere = 31
+let ageGrandPere = 72
+let ageUcle = 42
+console.log(ageEnfant);
+ console.log(agePre);
+ console.log(ageMere);
+ console.log(ageGrandPere);
+ console.log(ageUcle);
