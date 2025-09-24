4eec86abec4a91280e6496592d7af7509e927511
vanguleprince <vangu.leprince@gmail.com>
2025-09-24 16:03:23 +0100

    feat i'm finished my exercises introduction-java-script

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,30 @@
 // Exercice 8
 // Écrivez votre code ici
+
+let salaireMensuel = 500;
+
+let loyer = salaireMensuel * 0.30;
+
+let nourriture = salaireMensuel * 0.20;
+
+
+let transport = salaireMensuel * 0.10;
+
+
+let autresDepenses = 50;
+autresDepenses += 25;
+
+
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+
+
+let reste = salaireMensuel - totalDepenses;
+
+
+console.log("Salaire mensuel :", salaireMensuel, "€");
+console.log("Loyer :", loyer, "€");
+console.log("Nourriture :", nourriture, "€");
+console.log("Transport :", transport, "€");
+console.log("Autres dépenses :", autresDepenses, "€");
+console.log("Total des dépenses :", totalDepenses, "€");
+console.log("Reste après dépenses :", reste, "€");
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,2 +1,33 @@
 // Exercice 9
 // Écrivez votre code ici
+
+let total = 60000000 + 40000000 + 20000000; 
+
+
+let partEnfants = total * 0.75;
+let partFamille = total * 0.25; 
+
+
+let partEnfant = partEnfants / 3;
+
+
+let Paul = partEnfant;        
+let Marie = partEnfant;       
+let Eric = partEnfant / 2;    
+let Claire = partEnfant / 2;  
+
+
+let partFamilleMembre = partFamille / 3;
+
+let MadameMUKUNA = partFamilleMembre; 
+let Joseph = partFamilleMembre;       
+let Sarah = partFamilleMembre;        
+
+
+console.log("Paul :", Paul, "CDF");
+console.log("Marie :", Marie, "CDF");
+console.log("Eric :", Eric, "CDF");
+console.log("Claire :", Claire, "CDF");
+console.log("Madame MUKUNA :", MadameMUKUNA, "CDF");
+console.log("Joseph :", Joseph, "CDF");
+console.log("Sarah :", Sarah, "CDF");
